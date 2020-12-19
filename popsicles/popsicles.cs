79b5e59b14dad7a17d389e918b977013a15338fc
sasan zare <sasanzare75@gmail.com>
2020-12-19 19:19:53 +0330

    feat: Initial commit

--- popsicles/popsicles.cs
+++ popsicles/popsicles.cs
@@ -30,12 +30,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SoloLearn
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace SoloLearn{
+    class Program{
+        static void Main(string[] args){
             int siblings, popsicles;
             siblings = Convert.ToInt32(Console.ReadLine());
             popsicles = Convert.ToInt32(Console.ReadLine());
