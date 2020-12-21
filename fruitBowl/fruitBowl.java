e98bf90780092ccb2f39179d03e224edf3c33608
sasan zare <sasanzare75@gmail.com>
2020-12-21 20:52:51 +0330

    feat: Initial commit

---
+++ fruitBowl/fruitBowl.java
@@ -0,0 +1,45 @@
+/*
+
+======= Fruit Bowl =======
+
+======= sasan zare 21/12/2020 =======
+
+======= V 0.01 =======
+
+======= QUESTION =======
+
+You have a bowl on your counter with an even number of pieces of fruit in it. Half of them are bananas, and the other half are apples. You need 3 apples to make a pie.
+
+======= Task  =======
+
+Your task is to evaluate the total number of pies that you can make with the apples that are in your bowl given to total amount of fruit in the bowl.
+
+======= Input Format =======
+
+An integer that represents the total amount of fruit in the bowl.
+
+======= Output Format =======
+
+An integer representing the total number of whole apple pies that you can make.
+
+======= Sample Input =======
+
+26
+
+======= Sample Output =======
+
+4
+
+*/
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int fruit = input.nextInt();
+        System.out.println((fruit/2)/3);
+
+    }
+}
