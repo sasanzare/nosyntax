196a250f3e59c1c66e3549aab143d078a0fefca6
sasanzare <sasanzare75@gmail.com>
2025-08-06 11:32:04 +0330

    feat(pigLatin): Add C++ solution for Pig latin

---
+++ easy/pigLatin/pigLatin.cpp
@@ -0,0 +1,62 @@
+/*
+
+======= Pig Latin =======
+
+======= sasan zare 17/01/2021 =======
+
+======= V 0.01 =======
+
+======= QUESTION =======
+
+You have two friends who are speaking Pig Latin to each other! Pig Latin is the same words in the same order except that you take the first letter of each word and put it on the end, then you add 'ay' to the end of that. ("road" = "oadray")
+
+======= Task  =======
+
+Your task is to take a sentence in English and turn it into the same sentence in Pig Latin!
+
+======= Input Format =======
+
+A string of the sentence in English that you need to translate into Pig Latin. (no punctuation or capitalization)
+
+======= Output Format =======
+
+A string of the same sentence in Pig Latin.
+
+======= Sample Input =======
+
+"nevermind youve got them"
+
+======= Sample Output =======
+
+"evermindnay ouveyay otgay hemtay"
+
+
+*/
+
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+string pigLatin(string word) {
+    return word.substr(1) + word[0] + "ay";
+}
+
+int main() {
+    string input;
+    getline(cin, input);
+    stringstream ss(input);
+    string word;
+    bool first = true;
+    
+    while (ss >> word) {
+        if (!first) cout << " ";
+        cout << pigLatin(word);
+        first = false;
+    }
+    
+    return 0;
+}
