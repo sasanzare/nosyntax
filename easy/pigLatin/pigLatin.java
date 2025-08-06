bbd4227713824fae30330142a2c18d07ec4ec825
sasanzare <sasanzare75@gmail.com>
2025-08-06 11:42:31 +0330

    feat(pigLatin): Add Java solution for Pig latin

---
+++ easy/pigLatin/pigLatin.java
@@ -0,0 +1,50 @@
+/*
+
+======= Pig Latin =======
+
+======= sasan zare 17/01/2021 =======
+
+======= V 0.01 =======
+
+======= QUESTION =======
+
+You have two friends who are speaking Pig Latin to each other! Pig Latin is the same words in the same order except that you take the first letter of each word and put it on the end, then you add 'ay' to the end of that. ("road" = "oadray")
+
+======= Task  =======
+
+Your task is to take a sentence in English and turn it into the same sentence in Pig Latin!
+
+======= Input Format =======
+
+A string of the sentence in English that you need to translate into Pig Latin. (no punctuation or capitalization)
+
+======= Output Format =======
+
+A string of the same sentence in Pig Latin.
+
+======= Sample Input =======
+
+"nevermind youve got them"
+
+======= Sample Output =======
+
+"evermindnay ouveyay otgay hemtay"
+
+
+*/
+
+
+import java.util.Scanner;
+
+public class PigLatin {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String[] words = input.split(" ");
+        
+        for (int i = 0; i < words.length; i++) {
+            if (i > 0) System.out.print(" ");
+            System.out.print(words[i].substring(1) + words[i].charAt(0) + "ay");
+        }
+    }
+}
